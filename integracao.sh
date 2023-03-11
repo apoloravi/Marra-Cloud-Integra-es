90458e5513583990836f660427a2195bc248da4d
Marra Cloud <34797030+apoloravi@users.noreply.github.com>
2023-03-11 16:48:40 -0300

    Update integracao.sh
    Signed-off-by: Marra Cloud <34797030+apoloravi@users.noreply.github.com>

--- integracao.sh
+++ integracao.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 echo "Iniciando integração entre Mikrotik e Mk-auth..."
 
 # Configuração da VPN PPTP
@@ -8,34 +7,30 @@ echo "Configurando VPN PPTP..."
 
 # Acessando o Mikrotik via SSH
 echo "Acessando o Mikrotik via SSH..."
-ssh -p 2215 admin@172.20.1.2 << EOF
-
+ssh admin@172.20.1.2 -p 2215 <<EOF
 # Criando usuário no Mikrotik
 echo "Criando usuário no Mikrotik..."
-# Comandos para criar o usuário no Mikrotik
+/user add name=mk-auth password=123456 group=full
 
 # Configurando o Radius no Mikrotik
 echo "Configurando o Radius no Mikrotik..."
-# Comandos para configurar o Radius no Mikrotik
-/ip hotspot profile set 0 use-radius=yes
-/ip hotspot profile set 1 use-radius=yes
+/radius add service=ppp address=172.20.1.1 secret=senha12345
 
 # Baixando o arquivo chave.pub do Mk-auth
 echo "Baixando o arquivo chave.pub do Mk-auth..."
-/tool fetch url="http://172.20.1.1/chave.pub" mode=http dst-path=chave.pub
+/tool fetch url=http://172.20.1.1/admin/chave.pub mode=http
 
 # Importando o arquivo chave.pub no Mikrotik
 echo "Importando o arquivo chave.pub no Mikrotik..."
-/user ssh-keys import user=mkauth public-key-file=chave.pub
+/user ssh-keys import user=mk-auth public-key-file=chave.pub
 
 # Configurando permissões de IP para o usuário mkauth
 echo "Configurando permissões de IP para o usuário mkauth..."
-/ip firewall address-list add list=mkauth address=172.20.1.0/24
-/ip firewall address-list add list=mkauth address=192.99.159.81
+/user group add name=mk-auth policy="ssh,ftp,reboot,sensitive,read,test,write,policy,test,winbox,password,web,sniff,api,!local,!telnet,!ssh,!ftp,!web,!api,!winbox,!reboot,! sensitive,!read,!write,!policy"
 
 # Permitindo o uso do Radius no ppp secret
 echo "Permitindo o uso do Radius no ppp secret..."
-/radius incoming set accept=yes
+/ppp secret set [find] service=pppoe,hotspot,ovpn,vpn pptp-incoming-filter=accept
 
 EOF
 
