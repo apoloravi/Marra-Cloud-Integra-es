8de2b127fb4e892319f1cd8133004d05bc23c9f5
Marra Cloud <34797030+apoloravi@users.noreply.github.com>
2023-03-11 13:56:18 -0300

    Update integracao.sh
    Signed-off-by: Marra Cloud <34797030+apoloravi@users.noreply.github.com>

--- integracao.sh
+++ integracao.sh
@@ -8,7 +8,7 @@ echo "Configurando VPN PPTP..."
 
 # Acessando o Mikrotik via SSH
 echo "Acessando o Mikrotik via SSH..."
-ssh admin@172.20.1.2 -p 2215 <<EOF
+ssh -p 2215 admin@172.20.1.2 << EOF
 
 # Criando usuário no Mikrotik
 echo "Criando usuário no Mikrotik..."
@@ -17,26 +17,25 @@ echo "Criando usuário no Mikrotik..."
 # Configurando o Radius no Mikrotik
 echo "Configurando o Radius no Mikrotik..."
 # Comandos para configurar o Radius no Mikrotik
-/ip hotspot profile set [find default=yes] use-radius=yes
-/ip hotspot profile set [find default=yes] radius-interim-update=1m
+/ip hotspot profile set 0 use-radius=yes
+/ip hotspot profile set 1 use-radius=yes
 
 # Baixando o arquivo chave.pub do Mk-auth
 echo "Baixando o arquivo chave.pub do Mk-auth..."
-/tool fetch url=http://172.20.1.1/chave.pub
+/tool fetch url="http://172.20.1.1/chave.pub" mode=http dst-path=chave.pub
 
 # Importando o arquivo chave.pub no Mikrotik
 echo "Importando o arquivo chave.pub no Mikrotik..."
-/user ssh-keys import user=mk-auth public-key-file=chave.pub
+/user ssh-keys import user=mkauth public-key-file=chave.pub
 
 # Configurando permissões de IP para o usuário mkauth
 echo "Configurando permissões de IP para o usuário mkauth..."
-/ip firewall address-list add list=mkauth-address address=172.20.1.0/24 comment="Permissão para acesso do mkauth"
-/ip firewall address-list add list=mkauth-address address=192.99.159.81 comment="Permissão para acesso do mkauth"
+/ip firewall address-list add list=mkauth address=172.20.1.0/24
+/ip firewall address-list add list=mkauth address=192.99.159.81
 
 # Permitindo o uso do Radius no ppp secret
 echo "Permitindo o uso do Radius no ppp secret..."
-# Comandos para permitir o uso do Radius no ppp secret
-/ppp profile set default local-address=172.20.1.2 remote-address=ppp-remote use-radius=yes
+/radius incoming set accept=yes
 
 EOF
 
